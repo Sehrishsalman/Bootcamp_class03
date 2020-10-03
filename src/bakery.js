388d04b564fbf1f1c12ddb05293eb2251ec618aa
Sehrish Salman <sehrishsalman90@gamil.com>
2020-10-03 17:44:49 +0300

    update bakery.js

--- src/bakery.js
+++ src/bakery.js
@@ -3,8 +3,8 @@ import React from 'react'
 function Bakery(props){
     return(
       <div>  
-          <h1> We are serving today in savoury {props.DishName}</h1> 
-          <h1> We are serving today in Sweet {props.sweet}</h1> 
+          <h1> We are serving today in savoury. {props.DishName}</h1> 
+          <h1> We are serving today in Sweet. {props.sweet}</h1> 
 
       </div>
     )
